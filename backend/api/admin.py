5c9e56f3f352593f6a5958b8a4f100ce78258a99
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-06-05 19:29:23 +0200

    Register models

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+admin.site.register(
+    [
+        models.Company,
+        models.JobPost,
+        models.Technology,
+        models.Location,
+    ]
+)
