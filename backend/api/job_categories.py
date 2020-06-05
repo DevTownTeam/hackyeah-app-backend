9d7b13dee48b255d5422c67e0390e0fbeb87c1cf
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-06-05 19:16:43 +0200

    Create models

---
+++ backend/api/job_categories.py
@@ -0,0 +1,11 @@
+JOB_CATEGORIES_CHOICES = [
+    ('FE', 'Front End Development'),
+    ('BE', 'Back End Development'),
+    ('FS', 'Full Stack Development'),
+    ('MD', 'Mobile Development'),
+    ('DO', 'DevOps'),
+    ('QA', 'Quality Assurance'),
+    ('PM', 'Project Management'),
+    ('UI', 'User Interface'),
+    ('UX', 'User Experience'),
+]
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
+from .job_categories import JOB_CATEGORIES_CHOICES
 
+
+class Technology(models.Model):
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name_plural = 'Technologies'
+
+
+class JobCategory(models.Model):
+    name = models.CharField(choices=JOB_CATEGORIES_CHOICES, max_length=50)
+
+
+class Location(models.Model):
+    location = models.TextField()
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    technologies = models.ManyToManyField(Technology)
+    size = models.IntegerField(default=1)  # how many employees
+
+
+class JobPost(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    job_category = models.ForeignKey(JobCategory, on_delete=models.CASCADE)
+    technologies = models.ManyToManyField(Technology)
+    salary = models.IntegerField(default=0)
+
+    date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date', 'name']
