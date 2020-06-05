550f06855523eeb5cb319b6e66aa8506af342781
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-06-05 22:14:58 +0200

    Add more serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
-from .models import JobPost
-from rest_framework.renderers import JSONRenderer
 
+from .models import JobPost, Company, Location, Technology
 
 
 class JobPostSerializer(serializers.ModelSerializer):
@@ -9,3 +8,20 @@ class JobPostSerializer(serializers.ModelSerializer):
         model = JobPost
         fields = '__all__'
 
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = '__all__'
+
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = '__all__'
+
+
+class TechnologySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Technology
+        fields = '__all__'
