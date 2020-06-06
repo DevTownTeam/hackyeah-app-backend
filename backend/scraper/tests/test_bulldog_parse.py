0baaec480dc0f518cb10850d7332d1b6d9f480b1
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-06-06 18:58:34 +0200

    Add tests docstrings and a new test

--- backend/scraper/tests/test_bulldog_parse.py
+++ backend/scraper/tests/test_bulldog_parse.py
@@ -9,6 +9,7 @@ class BulldogParseTests(TestCase):
         self.parsed = parse_bulldog(JOBS)
 
     def test_includes_scrum_master(self):
+        """Check if the example data contains a Scrum Master position"""
         true_result = {'name': 'Scrum Master', 'company': 'Luxoft'}
         false_result = {'name': 'Master of Puppets', 'company': 'Metallica'}
 
@@ -16,3 +17,10 @@ class BulldogParseTests(TestCase):
 
         self.assertTrue(set(true_result.items()).issubset(superset))
         self.assertFalse(set(false_result.items()).issubset(superset))
+
+    def test_includes_description(self):
+        """Test that description is properly parsed"""
+        self.assertIn(
+            'We are looking for experienced individual who would be working as Scrum Master.',
+            self.parsed[0]['description']
+        )
