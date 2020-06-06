8395d9003a255a2a8fceceed4866d2e409fd65d9
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-06-06 08:44:21 +0200

    Add test_technologies_included

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
+from . import models
+
+
+class CompanyModelTests(TestCase):
+    def setUp(self):
+        technologies_list = ['JavaScript', 'React', 'Django', 'Docker']
+        self.technologies = [models.Technology.objects.get_or_create(name=name)[0] for name in technologies_list]
+        self.location = models.Location.objects.get_or_create(location='Pacanowo, ul. Koziołka Matołka 42')[0]
+        self.company = models.Company.objects.create(
+            name='Januszex sp. z o.o.',
+            location=self.location,
+            size=10
+        )
+        self.company.technologies.set(self.technologies)
+
+    def test_technologies_included(self):
+        """Test that technologies' names are included"""
+        technologies = [str(tech) for tech in self.company.technologies.all()]
+
+        self.assertIn('JavaScript', technologies)
+        self.assertEqual(len(self.company.technologies.all()), len(technologies))
